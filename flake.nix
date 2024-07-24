153023b75d3cc01ec84daf0ee9443a4a3166528c
sswoyer <54217314+barstoolbluz@users.noreply.github.com>
2024-07-24 15:38:09 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -7,8 +7,8 @@
 
   outputs = { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs allSystems;
     in
     {
       packages = forAllSystems (system:
@@ -17,31 +17,41 @@
             inherit system;
             config.allowUnfree = true;
           };
+          
+          supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
+          
+          vscodePackage = 
+            if (builtins.elem system supportedSystems) then
+              pkgs.vscode-with-extensions.override {
+                vscodeExtensions = with pkgs.vscode-extensions; [
+                  bbenoist.nix
+                  ms-python.python
+                  ms-azuretools.vscode-docker
+                ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+                  {
+                    name = "remote-ssh";
+                    publisher = "ms-vscode-remote";
+                    version = "0.102.0";
+                    sha256 = "sha256-YQ0Dy1C+xEGtwh0z97ypIMUq8D7PozVRb6xXUVZsjBw=";
+                  }
+                ];
+              }
+            else null;
         in
         {
-          default = pkgs.vscode-with-extensions.override {
-            vscodeExtensions = with pkgs.vscode-extensions; [
-              bbenoist.nix
-              ms-python.python
-              ms-azuretools.vscode-docker
-            ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-              {
-                name = "remote-ssh";
-                publisher = "ms-vscode-remote";
-                version = "0.102.0";
-                sha256 = "sha256-YQ0Dy1C+xEGtwh0z97ypIMUq8D7PozVRb6xXUVZsjBw=";
-              }
-            ];
-          };
+          default = vscodePackage;
         }
       );
 
-      # Add a devShell for each supported system
+      # Add a devShell for each system
       devShells = forAllSystems (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in {
+        let 
+          pkgs = nixpkgs.legacyPackages.${system};
+          vscodePackage = self.packages.${system}.default;
+        in 
+        {
           default = pkgs.mkShell {
-            buildInputs = [ self.packages.${system}.default ];
+            buildInputs = if vscodePackage != null then [ vscodePackage ] else [];
           };
         }
       );
