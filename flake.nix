0e20f222629a1cba7a425477213870b910c186ee
sswoyer <54217314+barstoolbluz@users.noreply.github.com>
2024-07-24 16:14:10 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,56 +1,40 @@
 {
-  description = "VSCode with a very specific set of extensions";
+  description = "VSCode for specific platforms";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs }: {
-    packages.x86_64-linux = let
-      pkgs = import nixpkgs {
-        config.allowUnfree = true;
-        system = "x86_64-linux";
+    packages = {
+      x86_64-linux = let
+        pkgs = import nixpkgs {
+          config.allowUnfree = true;
+          system = "x86_64-linux";
+        };
+      in
+      {
+        default = pkgs.vscode;
       };
-    in
-    {
-      default = pkgs.vscode-with-extensions.override {
-        vscodeExtensions = with pkgs.vscode-extensions; [
-          bbenoist.nix
-          ms-python.python
-          ms-azuretools.vscode-docker
-        ];
-      };
-    };
 
-    packages.x86_64-darwin = let
-      pkgs = import nixpkgs {
-        config.allowUnfree = true;
-        system = "x86_64-darwin";
+      x86_64-darwin = let
+        pkgs = import nixpkgs {
+          config.allowUnfree = true;
+          system = "x86_64-darwin";
+        };
+      in
+      {
+        default = pkgs.vscode;
       };
-    in
-    {
-      default = pkgs.vscode-with-extensions.override {
-        vscodeExtensions = with pkgs.vscode-extensions; [
-          bbenoist.nix
-          ms-python.python
-          ms-azuretools.vscode-docker
-        ];
-      };
-    };
 
-    packages.aarch64-darwin = let
-      pkgs = import nixpkgs {
-        config.allowUnfree = true;
-        system = "aarch64-darwin";
-      };
-    in
-    {
-      default = pkgs.vscode-with-extensions.override {
-        vscodeExtensions = with pkgs.vscode-extensions; [
-          bbenoist.nix
-          ms-python.python
-          ms-azuretools.vscode-docker
-        ];
+      aarch64-darwin = let
+        pkgs = import nixpkgs {
+          config.allowUnfree = true;
+          system = "aarch64-darwin";
+        };
+      in
+      {
+        default = pkgs.vscode;
       };
     };
   };
