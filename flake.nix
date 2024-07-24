cb81b363b78a1b89902074be6b9e02ebdab68dfe
sswoyer <54217314+barstoolbluz@users.noreply.github.com>
2024-07-24 15:16:52 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -5,25 +5,35 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-    packages = let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" ];
     in
-    builtins.genAttrs supportedSystems (system: let
-      pkgs = import nixpkgs {
-        config.allowUnfree = true;
-        inherit system;
-      };
-    in
-    rec {
-      default = vscode-with-extensions;
-      vscode-with-extensions = pkgs.vscode-with-extensions.override {
+    {
+      packages = builtins.genAttrs systems (system:
+        let
+          pkgs = import nixpkgs {
+            system = system;
+            config.allowUnfree = true;
+          };
+        in
+        {
+          defaultPackage = pkgs.vscode-with-extensions.override {
+            vscodeExtensions = with pkgs.vscode-extensions; [
+              bbenoist.nix
+              ms-python.python
+              ms-azuretools.vscode-docker
+            ];
+          };
+        }
+      );
+
+      defaultPackage = pkgs.vscode-with-extensions.override {
         vscodeExtensions = with pkgs.vscode-extensions; [
           bbenoist.nix
           ms-python.python
           ms-azuretools.vscode-docker
         ];
       };
-    });
-  };
+    };
 }
